89788ca4b1d92f53d52f5d813f2a0c00a88dd349
YangLong <phuong.dangdanh@ncc.asia>
2021-10-23 20:18:14 +0700

    Init

---
+++ js/about.js
@@ -0,0 +1,12 @@
+
+(function(global) {
+  'use strict';
+
+  try {
+    chrome.extension.getBackgroundPage().tgs.setViewGlobals(global);
+  } catch (e) {
+    window.setTimeout(() => window.location.reload(), 1000);
+    return;
+  }
+
+})(this);
